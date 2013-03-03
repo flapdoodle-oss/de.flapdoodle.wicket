9f6a67bb6ad4ffc337423fdfc45c76740c56cce9
Michael Mosmann <michael@mosmann.de>
2013-03-03 19:56:36 +0100

    generic crazy stuff works

--- debug/src/main/java/de/flapdoodle/tests/sample/Sample.java
+++ debug/src/main/java/de/flapdoodle/tests/sample/Sample.java
@@ -16,4 +16,33 @@ public class Sample<A, B, S extends ISample<B>> extends Simple<A> implements IJo
 	public static <A,B,N extends ISample<B>> IJoinedSample<A, B, N> of(A a,N next) {
 		return new Sample<A, B, N>(a, next);
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((_next == null)
+				? 0
+				: _next.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sample other = (Sample) obj;
+		if (_next == null) {
+			if (other._next != null)
+				return false;
+		} else if (!_next.equals(other._next))
+			return false;
+		return true;
+	}
+	
+	
 }
