6dfec84da9adb914d30ee59503657fb8b761853c
Michael Mosmann <michael@mosmann.de>
2026-09-01 08:08:56 +0200

    jetty servlet3 dependency update

--- examples/src/test/java/de/flapdoodle/wicket/examples/Start.java
+++ examples/src/test/java/de/flapdoodle/wicket/examples/Start.java
@@ -21,12 +21,13 @@
 package de.flapdoodle.wicket.examples;
 
 import org.apache.wicket.protocol.ws.javax.WicketServerEndpointConfig;
+import org.eclipse.jetty.ee10.webapp.WebAppContext;
+import org.eclipse.jetty.ee10.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
 import org.eclipse.jetty.jmx.MBeanContainer;
 import org.eclipse.jetty.server.*;
-import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.util.resource.Resource;
-import org.eclipse.jetty.webapp.WebAppContext;
-import org.eclipse.jetty.websocket.jakarta.server.config.JakartaWebSocketServletContainerInitializer;
+import org.eclipse.jetty.util.resource.ResourceFactory;
+import org.eclipse.jetty.util.ssl.SslContextFactory;
 
 import javax.management.MBeanServer;
 import java.lang.management.ManagementFactory;
@@ -54,7 +55,7 @@ public class Start {
 
         server.addConnector(http);
 
-        Resource keystore = Resource.newClassPathResource("/keystore");
+        Resource keystore = ResourceFactory.of(server).newClassLoaderResource("/keystore");
         if (keystore != null && keystore.exists())
         {
             // if a keystore for a SSL certificate is available, start a SSL
@@ -64,7 +65,7 @@ public class Start {
             // use this certificate anywhere important as the passwords are
             // available in the source.
 
-            org.eclipse.jetty.util.ssl.SslContextFactory.Server sslContextFactory = new org.eclipse.jetty.util.ssl.SslContextFactory.Server();
+            SslContextFactory.Server sslContextFactory = new SslContextFactory.Server();
             sslContextFactory.setKeyStoreResource(keystore);
             sslContextFactory.setKeyStorePassword("wicket");
             sslContextFactory.setKeyManagerPassword("wicket");
@@ -87,23 +88,18 @@ public class Start {
         }
 
         WebAppContext bb = new WebAppContext();
-        bb.setServer(server);
         bb.setContextPath("/");
         bb.setWar("src/main/webapp");
 
-        // uncomment the next two lines if you want to start Jetty with WebSocket (JSR-356) support
-        // you need org.apache.wicket:wicket-native-websocket-javax in the classpath!
-        // ServerContainer serverContainer = WebSocketServerContainerInitializer.configureContext(bb);
-        // serverContainer.addEndpoint(new WicketServerEndpointConfig());
-
-//		bb.getSessionHandler().setSessionCache(sessionCache);
-
-        ServletContextHandler contextHandler = ServletContextHandler.getServletContextHandler(bb.getServletContext());
-        JakartaWebSocketServletContainerInitializer.configure(contextHandler,
+        // WebSocket (JSR-356) support -- benoetigt
+        // org.apache.wicket:wicket-native-websocket-javax im Classpath.
+        // WebAppContext ist selbst ein ServletContextHandler, kann also direkt
+        // uebergeben werden.
+        JakartaWebSocketServletContainerInitializer.configure(bb,
           (servletContext, container) -> container.addEndpoint(new WicketServerEndpointConfig()));
+
         // uncomment next line if you want to test with JSESSIONID encoded in the urls
-        // ((AbstractSessionManager)
-        // bb.getSessionHandler().getSessionManager()).setUsingCookies(false);
+        // bb.getSessionHandler().getSessionManager().getSessionCookieConfig() ...
 
         server.setHandler(bb);
 
@@ -123,69 +119,4 @@ public class Start {
             System.exit(100);
         }
     }
-    
-//    public static void main(String[] args) throws Exception {
-//        int timeout = (int) Duration.ONE_HOUR.getMilliseconds();
-//
-//        Server server = new Server();
-//        SocketConnector connector = new SocketConnector();
-//
-//        // Set some timeout options to make debugging easier.
-//        connector.setMaxIdleTime(timeout);
-//        connector.setSoLingerTime(-1);
-//        connector.setPort(8080);
-//        server.addConnector(connector);
-//
-//		// check if a keystore for a SSL certificate is available, and
-//		// if so, start a SSL connector on port 8443. By default, the
-//		// quickstart comes with a Apache Wicket Quickstart Certificate
-//		// that expires about half way september 2021. Do not use this
-//		// certificate anywhere important as the passwords are available
-//		// in the source.
-//
-//        Resource keystore = Resource.newClassPathResource("/keystore");
-//        if (keystore != null && keystore.exists()) {
-//            connector.setConfidentialPort(8443);
-//
-//            SslContextFactory factory = new SslContextFactory();
-//            factory.setKeyStoreResource(keystore);
-//            factory.setKeyStorePassword("wicket");
-//            factory.setTrustStoreResource(keystore);
-//            factory.setKeyManagerPassword("wicket");
-//            SslSocketConnector sslConnector = new SslSocketConnector(factory);
-//            sslConnector.setMaxIdleTime(timeout);
-//            sslConnector.setPort(8443);
-//            sslConnector.setAcceptors(4);
-//            server.addConnector(sslConnector);
-//
-//            System.out.println("SSL access to the quickstart has been enabled on port 8443");
-//            System.out.println("You can access the application using SSL on https://localhost:8443");
-//            System.out.println();
-//        }
-//
-//        WebAppContext bb = new WebAppContext();
-//        bb.setServer(server);
-//        bb.setContextPath("/");
-//        bb.setWar("src/main/webapp");
-//
-//        // START JMX SERVER
-//        // MBeanServer mBeanServer = ManagementFactory.getPlatformMBeanServer();
-//        // MBeanContainer mBeanContainer = new MBeanContainer(mBeanServer);
-//        // server.getContainer().addEventListener(mBeanContainer);
-//        // mBeanContainer.start();
-//
-//        server.setHandler(bb);
-//
-//        try {
-//            System.out.println(">>> STARTING EMBEDDED JETTY SERVER, PRESS ANY KEY TO STOP");
-//            server.start();
-//            System.in.read();
-//            System.out.println(">>> STOPPING EMBEDDED JETTY SERVER");
-//            server.stop();
-//            server.join();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            System.exit(1);
-//        }
-//    }
 }
